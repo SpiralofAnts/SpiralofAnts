a599afb335443e29bd1589c46db1ea2389bc3ca8
SpiralofAnts <138092879+SpiralofAnts@users.noreply.github.com>
2023-07-01 09:21:47 -0500

    Scrolling Effect
    Got the scrolling effect from between levels to play in the background of the main content on the Cool Spot samples page.

---
+++ samples/coolspot/coolspot.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var bgElement = document.querySelector('.coolspot-bg');
+  
+  var scrollSpeed = 1; // Adjust the scrolling speed (increase or decrease the value as desired)
+  
+  var initHeight = bgElement.offsetTop;
+  
+  function scrollBackground() {
+    var currentScrollPos = bgElement.offsetTop;
+    var newScrollPos = currentScrollPos - scrollSpeed;
+    
+    if (newScrollPos < (initHeight - (2 * 256))) {
+      newScrollPos = -256;
+    }
+    
+    bgElement.style.top = newScrollPos + 'px';
+    
+    requestAnimationFrame(scrollBackground);
+  }
+  
+  scrollBackground();
+});
