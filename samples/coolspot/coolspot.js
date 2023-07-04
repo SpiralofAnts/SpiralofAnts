b8f69b6c2edb1c94549ff973c16212e8c5cf51e4
SpiralofAnts <138092879+SpiralofAnts@users.noreply.github.com>
2023-07-04 11:41:18 -0500

    Cool Spot Track Update
    I cleaned up some code as well, but the main point of this commit is to update the track that's showing on the Cool Spot page.

--- samples/coolspot/coolspot.js
+++ samples/coolspot/coolspot.js
@@ -1,22 +1,23 @@
 window.addEventListener('DOMContentLoaded', function() {
   var bgElement = document.querySelector('.coolspot-bg');
-  
-  var scrollSpeed = 1;
-  
+ 
+
+  var scrollSpeed1 = 1;
+
   var initHeight = bgElement.offsetTop;
-  
+
   function scrollBackground() {
-    var currentScrollPos = bgElement.offsetTop;
-    var newScrollPos = currentScrollPos - scrollSpeed;
-    
-    if (newScrollPos < (initHeight - (2 * 256))) {
-      newScrollPos = -256;
+    var currentScrollPos1 = bgElement.offsetTop;
+    var newScrollPos1 = currentScrollPos1 - scrollSpeed1;
+
+    if (newScrollPos1 < (initHeight - (2 * 256))) {
+      newScrollPos1 = -256;
     }
-    
-    bgElement.style.top = newScrollPos + 'px';
-    
+
+    bgElement.style.top = newScrollPos1 + 'px';
+
     requestAnimationFrame(scrollBackground);
   }
-  
+
   scrollBackground();
 });
